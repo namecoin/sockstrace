ee3abc1f5f83288c519e75accbe99d11f31d4738
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-12-05 20:24:17 +0300

    DetectIPandPort

--- testdata/ping.bash
+++ testdata/ping.bash
@@ -11,7 +11,7 @@ Check() {
     fi
 }
 
-git clone -b starting_tasks "$CIRRUS_REPO_CLONE_URL" && cd heteronculous-horklump
+git clone -b DetectIpAndPort "$CIRRUS_REPO_CLONE_URL" && cd heteronculous-horklump
 
 
 go run main.go >> dump.txt & sleep 10 ; Check
