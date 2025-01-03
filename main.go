2a21b645a89d69c6309d39f776322de25e028a19
Robert Mindo <mindo.robert1@gmail.com>
2025-01-03 07:15:16 -0500

    Add support for seccomp tracing

--- main.go
+++ main.go
@@ -229,6 +229,7 @@ func HandleConnect(task strace.Task, record *strace.TraceRecord, program *exec.C
 			exitAddr.Store(record.PID, IPPort)
 			log.Infof("Redirecting connections from %v to %v", IPPort, cfg.SocksTCP)
 			err := RedirectConns(record.Syscall.Args, cfg, record)
+
 			if err != nil {
 				return fmt.Errorf("failed to redirect connections: %w", err)
 			}
@@ -237,6 +238,7 @@ func HandleConnect(task strace.Task, record *strace.TraceRecord, program *exec.C
 			// Incase trans proxy will require a different implementation a switch will be used.
 			return nil
 		}
+
 		err := BlockSyscall(record.PID, IPPort)
 		if err != nil {
 			return fmt.Errorf("failed to block syscall for PID %d and IPPort %s: %w", record.PID, IPPort, err)
@@ -493,6 +495,7 @@ func Socksify(args strace.SyscallArguments, record *strace.TraceRecord, t strace
 	case "socks5":
 		const timeout = 10
 		cl, err := socks5.NewClient(IPPort, username, password, timeout, timeout)
+
 		if err != nil {
 			return err
 		}
