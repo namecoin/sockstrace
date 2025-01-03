8dc65fb3dbb0109b27d38a53d09f1467ff6cecdc
Robert Mindo <mindo.robert1@gmail.com>
2025-01-03 06:59:59 -0500

    Add support for seccomp tracing

--- main.go
+++ main.go
@@ -229,6 +229,7 @@ func HandleConnect(task strace.Task, record *strace.TraceRecord, program *exec.C
 			exitAddr.Store(record.PID, IPPort)
 			log.Infof("Redirecting connections from %v to %v", IPPort, cfg.SocksTCP)
 			err := RedirectConns(record.Syscall.Args, cfg, record)
+
 			if err != nil {
 				return fmt.Errorf("failed to redirect connections: %w", err)
 			}
@@ -237,7 +238,6 @@ func HandleConnect(task strace.Task, record *strace.TraceRecord, program *exec.C
 			// TODO: handle invalid flag
 			// Incase trans proxy will require a different implementation a switch will be used.
 		}
-
 		err := BlockSyscall(record.PID, IPPort)
 		if err != nil {
 			return fmt.Errorf("failed to block syscall for PID %d and IPPort %s: %w", record.PID, IPPort, err)
@@ -247,7 +247,7 @@ func HandleConnect(task strace.Task, record *strace.TraceRecord, program *exec.C
 	return nil
 }
 
-// ParseAddress reads an sockaddr struct from the given address and converts it
+// ParseAddress reads an sockaddr struct from the given address and converts it //nolint
 // to the FullAddress format. It supports AF_UNIX, AF_INET and AF_INET6
 // addresses
 func ParseAddress(t strace.Task, args strace.SyscallArguments) (FullAddress, error) { //nolint
