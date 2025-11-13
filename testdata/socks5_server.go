130c490ca24520e99ea4b93c525a0f2c868f08c6
Robert <104002271+robertmin1@users.noreply.github.com>
2025-11-13 13:37:32 +0300

    Add WebRTC Firefox Test task to Cirrus CI
    Add WebRTC Firefox Test task to Cirrus CI

--- testdata/socks5_server.go
+++ testdata/socks5_server.go
@@ -1,30 +1,41 @@
 package main
 
 import (
-	"log"
+    "log"
+    "os"
 
-	"github.com/armon/go-socks5"
+    "github.com/armon/go-socks5"
 )
 
 func main() {
-	// Define username and password
-	creds := socks5.StaticCredentials{
-		"user": "pass",
-	}
+    disableAuth := os.Getenv("NO_AUTH") == "1"
 
-	auth := socks5.UserPassAuthenticator{Credentials: creds}
+    var conf *socks5.Config
 
-	conf := &socks5.Config{
-		AuthMethods: []socks5.Authenticator{auth},
-	}
+    if disableAuth {
+        log.Println("Starting SOCKS5 server with NO authentication (NO_AUTH=1)")
+        conf = &socks5.Config{} // no auth
+    } else {
+        // Hardcoded credentials
+        creds := socks5.StaticCredentials{
+            "user": "pass",
+        }
 
-	server, err := socks5.New(conf)
-	if err != nil {
-		log.Fatalf("Error creating SOCKS5 server: %v", err)
-	}
+        auth := socks5.UserPassAuthenticator{Credentials: creds}
+        conf = &socks5.Config{
+            AuthMethods: []socks5.Authenticator{auth},
+        }
 
-	log.Println("SOCKS5 proxy listening on 127.0.0.1:9050 (user=user, pass=pass)")
-	if err := server.ListenAndServe("tcp", "127.0.0.1:9050"); err != nil {
-		log.Fatalf("Error starting SOCKS5 server: %v", err)
-	}
+        log.Println("Starting SOCKS5 proxy with authentication (user=user, pass=pass)")
+    }
+
+    server, err := socks5.New(conf)
+    if err != nil {
+        log.Fatalf("Error creating SOCKS5 server: %v", err)
+    }
+
+    log.Println("SOCKS5 proxy listening on 127.0.0.1:9050")
+    if err := server.ListenAndServe("tcp", "127.0.0.1:9050"); err != nil {
+        log.Fatalf("Error starting SOCKS5 server: %v", err)
+    }
 }
